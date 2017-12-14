d3b4d4ef57a1e03b04995271bd5ba447db7afdda
Nick Felker <fleker@google.com>
2017-12-14 14:07:46 -0800

    Initial commit of Node.JS sample
    Example of using Google Assistant in Node.JS
    
    Change-Id: I51f97e188d5396306eba7fe29507324966c80765

---
+++ README.md
@@ -0,0 +1,74 @@
+# Assistant SDK Sample for Node.JS
+
+## Setup
+
+1. Create or open a project in the [Actions Console](http://console.actions.google.com)
+1. Follow the instructions to [register a device model](https://developers.google.com/assistant/sdk/guides/service/python/embed/register-device)
+  1. Download `credentials.json`
+1. Install the [`google-oauthlib-tool`][google-oauthlib-tool] in a [Python 3][python3] virtual environment:
+
+```
+python3 -m venv env
+env/bin/python -m pip install --upgrade pip setuptools
+env/bin/pip install --upgrade google-auth-oauthlib[tool]
+```
+1. Use the [`google-oauthlib-tool`][google-oauthlib-tool] to generate credentials:
+
+```
+env/bin/google-oauthlib-tool --client-secrets credentials.json \
+                             --credentials devicecredentials.json \
+                             --scope https://www.googleapis.com/auth/assistant-sdk-prototype \
+                             --save
+```
+
+1. Run `cd google-assistant-grpc`
+1. Run `npm install`
+1. Run `node googleassistant.js`
+
+```Javascript
+const GoogleAssistant = require('./googleassistant');
+const deviceCredentials = require('./devicecredentials.json');
+
+const CREDENTIALS = {
+  client_id: deviceCredentials.client_id,
+  client_secret: deviceCredentials.client_secret,
+  refresh_token: deviceCredentials.refresh_token,
+  type: "authorized_user"
+};
+
+const assistant = new GoogleAssistant(CREDENTIALS);
+```
+
+## Usage
+
+The `GoogleAssistant` class has several methods and helpers.
+
+* `.assist(string)` - Sends a string request to the Assistant. Returns a promise with the text response or device action if applicable.
+* `.setDeviceConfig(modelid, instanceid)` - Sets the device model and instance. This will need to be set with the model id you defined in the Actions Console to use device actions.
+* `.startConversation([initialPrompt])` - Starts a conversation with "my test app". Returns a promise with the text response.
+* `.startConversationWith(appName, [initialPrompt])` - Starts a conversation with an Assistant app. Returns a promise with the text response.
+* `.endConversation()` - Ends a conversation with an Assistant app. Returns a promise.
+* `.setLocale(locale)` - Changes the Assistant locale, in the format **en-US**.
+
+```Javascript
+assistant.assist('what time is it')
+  .then(({ text }) => {
+    console.log(text); // Will log "It's 12:30"
+  });
+```
+
+You can chain together several requests and responses with Promises, as shown below.
+
+```Javascript
+assistant.assist('what is the weather')
+  .then(({ text }) => {
+    console.log(text);
+    return assistant.assist('what about the weather tomorrow')
+  })
+  .then(({ text }) => {
+    console.log(text);
+  });
+```
+
+## License
+See `LICENSE`.
---
+++ google-assistant-grpc/googleassistant.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+const path = require('path');
+const grpc = require('grpc');
+const protoFiles = require('google-proto-files');
+const resolve = require('resolve');
+const GoogleAuth = require('google-auth-library');
+
+const PROTO_ROOT_DIR = protoFiles('..');
+const embedded_assistant_pb = grpc.load({
+    root: PROTO_ROOT_DIR,
+    file: path.relative(PROTO_ROOT_DIR, protoFiles.embeddedAssistant.v1alpha2)
+}).google.assistant.embedded.v1alpha2;
+
+class GoogleAssistant {
+    constructor(credentials) {
+        GoogleAssistant.prototype.endpoint_ = "embeddedassistant.googleapis.com";
+        this.client = this.createClient_(credentials);
+        this.locale = "en-US";
+        this.deviceModelId = 'default';
+        this.deviceInstanceId = 'default';
+    }
+    createClient_(credentials) {
+        const sslCreds = grpc.credentials.createSsl();
+        // https://github.com/google/google-auth-library-nodejs/blob/master/ts/lib/auth/refreshclient.ts
+        const auth = new GoogleAuth();
+        const refresh = new auth.UserRefreshClient();
+        refresh.fromJSON(credentials, function (res) { });
+        const callCreds = grpc.credentials.createFromGoogleCredential(refresh);
+        const combinedCreds = grpc.credentials.combineChannelCredentials(sslCreds, callCreds);
+        const client = new embedded_assistant_pb.EmbeddedAssistant(this.endpoint_, combinedCreds);
+        return client;
+    }
+
+    assist(input) {
+        const config = new embedded_assistant_pb.AssistConfig();
+        config.setTextQuery(input);
+        config.setAudioOutConfig(new embedded_assistant_pb.AudioOutConfig());
+        config.getAudioOutConfig().setEncoding(1);
+        config.getAudioOutConfig().setSampleRateHertz(16000);
+        config.getAudioOutConfig().setVolumePercentage(100);
+        config.setDialogStateIn(new embedded_assistant_pb.DialogStateIn());
+        config.setDeviceConfig(new embedded_assistant_pb.DeviceConfig());
+        config.getDialogStateIn().setLanguageCode(this.locale);
+        config.getDeviceConfig().setDeviceId(this.deviceInstanceId);
+        config.getDeviceConfig().setDeviceModelId(this.deviceModelId);
+        const request = new embedded_assistant_pb.AssistRequest();
+        request.setConfig(config);
+
+        delete request.audio_in;
+        const conversation = this.client.assist();
+        return new Promise((resolve, reject) => {
+            let response = {};
+            conversation.on('data', (data) => {
+                if (data.device_action) {
+                    response.deviceAction = JSON.parse(data.device_action.device_request_json);
+                } else if (data.dialog_state_out !== null && data.dialog_state_out.supplemental_display_text) {
+                    response.text = data.dialog_state_out.supplemental_display_text;
+                }
+            });
+            conversation.on('end', (error) => {
+                // Response ended, resolve with the whole response.
+                resolve(response);
+            });
+            conversation.on('error', (error) => {
+                reject(error);
+            });
+            conversation.write(request);
+            conversation.end();
+        });
+    }
+}
+
+const deviceCredentials = require('./devicecredentials.json');
+
+const CREDENTIALS = {
+    client_id: deviceCredentials.client_id,
+    client_secret: deviceCredentials.client_secret,
+    refresh_token: deviceCredentials.refresh_token,
+    type: "authorized_user"
+};
+
+const assistant = new GoogleAssistant(CREDENTIALS);
+const stdio = require('stdio');
+// Allow user to continually input questions and receive answers.
+const promptUser = () => {
+    stdio.question('> ', (err, prompt) => {
+        assistant.assist(prompt)
+            .then(({ text }) => {
+                console.log(text); // Will log the answer
+                promptUser();
+            });
+    });
+};
+
+promptUser();
+module.exports = GoogleAssistant;
